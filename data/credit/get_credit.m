34a3de66174ac044e56b84d5b3f4831c3199a9aa
Wouter M. Kouw <wmkouw@gmail.com>
2017-05-14 12:22:36 +0200

    Added credit-getter

---
+++ data/credit/get_credit.m
@@ -0,0 +1,26 @@
+function [D,y] = get_credit(varargin)
+% Script to download credit screening dataset from UCI machine learning repository
+
+% Parse
+p = inputParser;
+addOptional(p, 'save', false);
+parse(p, varargin{:});
+
+%% Start downloading files
+
+fprintf('Starting downloads..')
+websave('credit.data', ...
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data');
+websave('credit.names', ...
+    'https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.names');
+
+fprintf('Done \n')
+
+%% Call parse script
+[D,y] = parse_credit_gen('save', p.Results.save);
+
+end
+
+
+
+
