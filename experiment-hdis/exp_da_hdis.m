9cf0f4a0ee25f0cf98186bb3d50091fd89a0a26e
Wouter M. Kouw <wmkouw@gmail.com>
2017-05-15 19:27:00 +0200

    Re-organized experiments-hdis

--- experiment-hdis/exp_da_hdis.m
+++ experiment-hdis/exp_da_hdis.m
@@ -4,7 +4,7 @@ function exp_da_hdis(varargin)
 % Parse hyperparameters
 p = inputParser;
 addOptional(p, 'cix', []);
-addOptional(p, 'clf', 'slda');
+addOptional(p, 'clf', 'tcp-ls');
 addOptional(p, 'nC', 3);
 addOptional(p, 'nR', 1);
 addOptional(p, 'nF', 5);
@@ -12,6 +12,7 @@ addOptional(p, 'lr', 'geom');
 addOptional(p, 'prep', {''});
 addOptional(p, 'maxIter', 1e4);
 addOptional(p, 'xTol', 1e-10);
+addOptional(p, 'mu', 1);
 addOptional(p, 'alpha', 1);
 addOptional(p, 'lambda', []);
 addOptional(p, 'gamma', .1);
@@ -56,18 +57,8 @@ for n = cmbl
     ixS = domains(cc(n,1))+1:domains(cc(n,1)+1);
     ixT = domains(cc(n,2))+1:domains(cc(n,2)+1);
     
-    % Run estimator
+    % Select adaptive approach
     switch p.Results.clf
-        
-        % Source classifiers
-        case 'slda'
-            exp_da_sda(D(ixS, :),y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda, 'clf', 'lda');
-        case 'sqda'
-            exp_da_sda(D(ixS, :),y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda, 'clf', 'qda');
-        case 'sls'
-            exp_da_sls(D(ixS, :),y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda);
-            
-        % Domain-adaptive classifiers
         case 'tcp-lda'
             exp_da_tcp(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'alpha', p.Results.alpha, 'lambda', p.Results.lambda, 'clf', 'tcp-lda', 'lr', p.Results.lr);
         case 'tcp-qda'
@@ -75,23 +66,15 @@ for n = cmbl
         case 'tcp-ls'
             exp_da_tcp(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'alpha', p.Results.alpha, 'lambda', p.Results.lambda, 'clf', 'tcp-ls');
         case 'kmm-lsq'
-            exp_da_iwc(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'iwe', 'kmm', 'clf', 'lsq');
+            exp_da_iwc(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'iwe', 'kmm', 'clf', 'lsq', 'gamma', p.Results.gamma);
         case 'kmm-lr'
-            exp_da_iwc(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'iwe', 'kmm', 'clf', 'lr');
+            exp_da_iwc(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'iwe', 'kmm', 'clf', 'lr', 'gamma', p.Results.gamma);
         case 'rba'
             exp_da_rba(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'gamma', p.Results.gamma, 'lambda', p.Results.lambda,'clip', p.Results.clip, 'iwe', p.Results.iwe);
         case 'rcsa'
             exp_da_rcsa(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'useGamma', p.Results.useGamma, 'lambda', p.Results.lambda);
         case 'tca'
-            exp_da_tca(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda', p.Results.lambda, 'nC', p.Results.nC);
-            
-        % Target classifiers
-        case 'tlda'
-            exp_da_tda(D(ixT,:), y(ixT), 'NM', p.Results.NM, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda, 'clf', 'lda');
-        case 'tqda'
-            exp_da_tda(D(ixT,:), y(ixT), 'NM', p.Results.NM, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda, 'clf', 'qda');
-        case 'tls'
-            exp_da_tls(D(ixT, :),y(ixT),'NM', p.Results.NM, 'nR', p.Results.nR, 'nF', p.Results.nF, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda',p.Results.lambda);
+            exp_da_tca(D(ixS,:), y(ixS), D(ixT,:), y(ixT), 'NN', p.Results.NN, 'nR', p.Results.nR, 'nF', p.Results.nF, 'maxIter', p.Results.maxIter, 'xTol', p.Results.xTol, 'saveName', [p.Results.saveName p.Results.dataName '_prep' p.Results.prep{logical(cellfun(@isstr, p.Results.prep))}  '_cc' sprintf('%1i', n) '_nR' num2str(p.Results.nR) '_'], 'lambda', p.Results.lambda, 'nC', p.Results.nC, 'mu', p.Results.mu,  'sigma', p.Results.sigma);
         otherwise
             disp(['Classifier not recognized']);
     end
--- experiment-hdis/run_exp_da_hdis.m
+++ experiment-hdis/run_exp_da_hdis.m
@@ -10,6 +10,8 @@ nR = 1;
 % Hyperparameters
 lambda = 1e-3;
 alpha = 2;
+mu = 1./2;
+nC = 8;
 
 % Optimization parameters
 xTol = 1e-8;
@@ -24,6 +26,7 @@ clfs = {'tca', 'kmm-lsq', 'rcsa', 'rba', 'tcp-ls', 'tcp-lda', 'tcp-qda'};
 for c = 1:length(clfs)
 
     exp_da_hdis('prep', prep, 'nR', nR, 'clf', clfs{c}, ...
-        'xTol', xTol, 'maxIter', maxIter, 'lambda', lambda, 'alpha', alpha, ...
+        'xTol', xTol, 'maxIter', maxIter, ...
+        'lambda', lambda, 'alpha', alpha, 'mu', mu, 'nC', nC, ...
         'dataName', 'heart_disease', 'saveName', saveName);
 end
